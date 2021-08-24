ae78372f9ec8aa429b6d44e6a4210f7e738b1855
moazzamsyntax <moazzamsyntax@gmail.com>
2021-08-24 03:28:18 -0400

    1st commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //write down the code that prints integers from 1 to 10
+         for (int i = 1; i <= 10; i++)
+                System.out.println(i);
+
+    }
+}
