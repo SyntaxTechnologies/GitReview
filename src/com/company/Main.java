3b6ac4c506200ebeae64618540a8087bbaa4b3d2
moazzamsyntax <moazzamsyntax@gmail.com>
2021-08-24 03:57:04 -0400

    2nd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        //write down the code that prints integers from 1 to 10
-         for (int i = 1; i <= 10; i++)
+        //write down the code that prints integers from 1 to 100
+         for (int i = 1; i <= 100; i++)
                 System.out.println(i);
 
     }
